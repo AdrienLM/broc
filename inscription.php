d1dac1940643236eac5caf75d2b2f6763e4ab409
Emma <Emma.Colin.Etu@univ-lemans.fr>
2018-11-26 17:28:15 +0100

    Presque fini
    il manque juste les icones des input

--- inscription.php
+++ inscription.php
@@ -71,17 +71,33 @@ if(isset($_POST['forminscription']))
 
 ?>
 
-
-
-    <html lang="fr">
-
+<html lang="fr">
     <head>
-        <title>KLEIZ | INSCRIPTION</title>
         <meta charset="utf-8">
+        <title>KLEIZ | INSCRIPTION</title>
+        <meta name="description" content="">
+        <link rel="stylesheet" href="css/styleInscription.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <link rel="apple-touch-icon" sizes="57x57" href="images/favicon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="images/favicon/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="images/favicon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="images/favicon/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="images/favicon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="images/favicon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="images/favicon/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="images/favicon/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="images/favicon/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="images/favicon/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+        <link rel="manifest" href="images/favicon/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="images/favicon/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
     </head>
-
     <body>
-
+        <section>
             <?php
 			if(isset($erreur)) 
 			{
@@ -94,53 +110,44 @@ if(isset($_POST['forminscription']))
 				echo '<div class="messageV animated bounceIn"><img class="logoError" src="image/valide.svg" width="20" height="20" />' .$valide.  '</div>';
 			}
 			?>
-                    <form method="POST" action="">
-
-                        <ul class="form-style-1">
-                            <li><label for="pseudo"></label><input id="pseudo" type="text" name="pseudo" class="field-divided" title="Caractères spéciaux interdits" pattern="^([0-9a-zA-Z]{3,15})$" placeholder="Pseudo" value="<?php if(isset($pseudo)) { echo $pseudo; } ?>" /></li>
-                            <li>
-                                <label for="mail"></label>
-                                <input id="email" type="email" name="mail" class="field-long" placeholder="Email" onpaste="return false;" required="required" pattern="^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$" title="Adresse mail trop courte" value="<?php if(isset($mail)) { echo $mail; } ?>" />
-                            </li>
-                            <li>
-                                <label for="mail2"></label>
-                                <input id="email" type="email" name="mail2" class="field-long" placeholder="Confirmation Email" onpaste="return false;" required="required" pattern="^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$" title="Adresse mail trop courte" value="<?php if(isset($mail2)) { echo $mail2; } ?>" />
-                            </li>
-                            <li>
-                                <label for="mdp"></label>
-                                <input id="mdp" type="password" name="mdp" minlength="6" maxlength="32" class="field-long" onpaste="return false;" required="required" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,32}$" placeholder="Mot de passe" title="Votre mot de passe doit contenir au moins une majuscule, une miniscule et un chiffre" />
-                            </li>
-                            <li>
-                                <label for="mdp2"></label>
-                                <input id="mdp2" type="password" name="mdp2" minlength="6" maxlength="32" class="field-long" onpaste="return false;" required="required" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,32}$" placeholder="Confirmation du mot de passe" title="Votre mot de passe doit contenir au moins une majuscule, une miniscule et un chiffre" />
-                            </li>
-                            <div class="check">
-                                <label class="container">J'accepte les <a href="conditionUtilisation.php">conditions générales d'utilisation de Ureka</a>
-                                    <input type="checkbox" required>
-                                   <span class="checkmark"></span>
-                               </label>
-
-                            </div>
-                            <li class="centrer">
-
-
-                                <input type="submit" name="forminscription" value="S'inscrire" class="boutonInput" />
-
-
-                            </li>
-                        </ul>
-                    </form>
-
-
-
-
-
-
-
-
-
-        </div>
-
+           <img src="images/logo.png" alt="logo kleiz" id="logo">
+           <div id="titre">
+               <h2>Inscription</h2>
+               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do. Consectetur adipiscing elit, sed do.</p>
+           </div>
+            <form method="POST" action="">
+                <ul class="form-style-1">
+                    <li><label for="pseudo"></label><input id="pseudo" type="text" name="pseudo" class="field-divided" title="Caractères spéciaux interdits" pattern="^([0-9a-zA-Z]{3,15})$" placeholder="Pseudo" value="<?php if(isset($pseudo)) { echo $pseudo; } ?>" /></li>
+                    <li>
+                        <label for="mail"></label>
+                        <input id="email" type="email" name="mail" class="field-long" placeholder="Email" onpaste="return false;" required="required" pattern="^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$" title="Adresse mail trop courte" value="<?php if(isset($mail)) { echo $mail; } ?>" />
+                    </li>
+                    <li>
+                        <label for="mail2"></label>
+                        <input id="email" type="email" name="mail2" class="field-long" placeholder="Confirmation Email" onpaste="return false;" required="required" pattern="^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$" title="Adresse mail trop courte" value="<?php if(isset($mail2)) { echo $mail2; } ?>" />
+                    </li>
+                    <li>
+                        <label for="mdp"></label>
+                        <input id="mdp" type="password" name="mdp" minlength="6" maxlength="32" class="field-long" onpaste="return false;" required="required" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,32}$" placeholder="Mot de passe" title="Votre mot de passe doit contenir au moins une majuscule, une miniscule et un chiffre" />
+                    </li>
+                    <li>
+                        <label for="mdp2"></label>
+                        <input id="mdp2" type="password" name="mdp2" minlength="6" maxlength="32" class="field-long" onpaste="return false;" required="required" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,32}$" placeholder="Confirmation du mot de passe" title="Votre mot de passe doit contenir au moins une majuscule, une miniscule et un chiffre" />
+                    </li>
+                    <div class="check">
+                        <label class="container">J'accepte les <a href="conditionUtilisation.php">conditions générales d'utilisation de Kleiz</a>
+                            <input type="checkbox" required>
+                           <span class="checkmark"></span>
+                       </label>
+                    </div>
+                    <li class="centrer">
+                        <input type="submit" name="forminscription" value="S'inscrire &nbsp; &nbsp; &rarr;" class="boutonInput" />
+                    </li>
+                </ul>
+            </form>
+        </section>
+        <section>
+            <h1>Les legendes de <span>Broceliande</span></h1>
+        </section>
     </body>
-
-    </html>
+</html>
---
+++ param.inc.php
@@ -0,0 +1,11 @@
+<?php
+	if(!defined("MYHOST"))
+		define("MYHOST","127.0.0.1");
+	if(!defined("MYUSER"))
+		define("MYUSER","root");
+	if(!defined("MYPASS"))
+		define("MYPASS","");
+	if(!defined("MYDB"))
+		define("MYDB","18mmi2pj02");
+?>
+
