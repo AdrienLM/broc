f8efc4b82e6845834516a5d4d233d1d1fe72ab46
linkerdu53 <q.wascheul@gmail.com>
2018-11-27 16:01:55 +0100

    avancement
    un peu pour save etat pour le moment

--- genererHerbier.php
+++ genererHerbier.php
@@ -13,14 +13,11 @@ $manager = new HerbierManage($pdo);   // creation de la gestion des Herbiers pou
 $manager->deleteAll();
 
 $Herbier[1] = new Herbier([   // Mise en mémoire de l'instant 
-  'nom' => 'Fougère',
-  'image' => 'images/fougerre.png',
-  'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
-, sunt in culpa qui officia deserunt mollit anim id est laborum',
+  'nom' => 'Feuille d\'or',
+  'image' => 'images/feuille1Or.png',
+  'description' => 'Les feuilles d’or apparaissent chaque nuit sur les branches de l’arbre d’or. Les lutins venaient chaque matin le récolter pour leurs propriétés magiques. Cette potion permettrait notamment de ramener à la vie les arbres meurtris.',
   'position' => 1,
-  'imageUnlock' => 'images/fougerreUnlock.png',
+  'imageUnlock' => 'images/feuille1OrUnlock.png',
   ]);
 
 
@@ -29,14 +26,11 @@ $manager->add($Herbier[1]);	// Ajout d'une Herbier
 
 
 $Herbier[2] = new Herbier([
-  'nom' => 'gantiane',
-  'image' => 'images/gantiane.png',
-  'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
-, sunt in culpa qui officia deserunt mollit anim id est laborum',
+  'nom' => 'Fougère',
+  'image' => 'images/fougerre.png',
+  'description' => 'L’Osmonde royale est une fougère que l’on peut trouver en abondance en Bretagne. Ses plus grandes feuilles peuvent former des touffes jusqu’à deux mètres. Elle tient son nom de l’histoire d’Osmund le batelier et de sa famille, sauvés durant l’invasion danoise en se cachant dans ses feuilles.',
   'position' => 2,
-  'imageUnlock' => 'images/gantianeUnlock.png',
+  'imageUnlock' => 'images/fougerreUnlock.png',
   ]);
 
 
@@ -45,14 +39,11 @@ $manager->add($Herbier[2]); // Ajout d'une Herbier
 
 
 $Herbier[3] = new Herbier([
-  'nom' => 'gantiane',
-  'image' => 'images/gantiane.png',
-  'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
-, sunt in culpa qui officia deserunt mollit anim id est laborum',
+  'nom' => 'Gentiane',
+  'image' => 'images/gentiane.png',
+  'description' => 'Les gentianes sont des fleurs sauvages vivaces et rustiques. Étant principalement des fleurs de montagne, elles poussent dans les endroits frais. La gentiane offre de nombreux bienfaits, c’est un tonifiant efficace qui prévient le vieillissement et maintient le bon état du système immunitaire.',
   'position' => 3,
-  'imageUnlock' => 'images/gantianeUnlock.png',
+  'imageUnlock' => 'images/gentianeUnlock.png',
   ]);
 
 
@@ -61,14 +52,12 @@ $manager->add($Herbier[3]); // Ajout d'une Herbier
 
 
 $Herbier[4] = new Herbier([
-  'nom' => 'gantiane',
-  'image' => 'images/gantiane.png',
+  'nom' => 'A venir',
+  'image' => 'images/aVenir.png',
   'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
-, sunt in culpa qui officia deserunt mollit anim id est laborum',
+aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
   'position' => 4,
-  'imageUnlock' => 'images/gantianeUnlock.png',
+  'imageUnlock' => 'images/aVenir.png',
   ]);
 
 
@@ -77,14 +66,12 @@ $manager->add($Herbier[4]); // Ajout d'une Herbier
 
 
 $Herbier[5] = new Herbier([
-  'nom' => 'gantiane',
-  'image' => 'images/gantiane.png',
+  'nom' => 'A venir',
+  'image' => 'images/aVenir.png',
   'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
-aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
-, sunt in culpa qui officia deserunt mollit anim id est laborum',
+aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
   'position' => 5,
-  'imageUnlock' => 'images/gantianeUnlock.png',
+  'imageUnlock' => 'images/aVenir.png',
   ]);
 
 
