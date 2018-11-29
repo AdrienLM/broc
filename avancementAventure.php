8a5fd5b1a06e305e13f6da3698bcd6c979582da3
linkerdu53 <q.wascheul@gmail.com>
2018-11-29 10:15:07 +0100

    ajout update Aventure en php
    poi

--- avancementAventure.php
+++ avancementAventure.php
@@ -7,53 +7,80 @@ $debugQWMain = false;
     $requete = "SELECT AvancementUser FROM membres WHERE IdUser = ".$idCompte;
     if($debugQWMain) echo $requete;
     $ifAvancementUserExist = $pdo->query($requete);
-    //echo ''.$ifAvancementUserExist.'';
     $AvancementUserExist = $ifAvancementUserExist->fetch(PDO::FETCH_ASSOC);
     if($debugQWMain) echo '<br />'.$AvancementUserExist["AvancementUser"];
 
 
 
 if($AvancementUserExist["AvancementUser"] != null)
-{   
-    if($debugQWMain) echo '<br />'.'existe dans if ';
+{    
+        if($debugQWMain) echo '<br />'.'existe dans if ';
 }else{
-    if($debugQWMain) echo '<br />'.'existe pas else';
-    
-    $modifdata = $pdo->prepare('UPDATE membres SET AvancementUser= :numAvance WHERE IdUser= :idCompte');
-try
-{
-    $numAvancement = 1;
-	//$modifdata->bindParam(':datedec', $_POST['datedec'], PDO:: PARAM_STR); // date
-	$modifdata->bindParam(':numAvance', $numAvancement, PDO:: PARAM_INT); // entier
-	$modifdata->bindParam(':idCompte', $idCompte, PDO:: PARAM_INT); // entier
-	$modifdata->execute();
-	//header ('location: avancementAventure.php');
-}
-catch ( Exception $e )
-{
-	if($debugQWMain) echo 'Erreur de requête : ', $e->getMessage();
-    //header ('location: accueil.php');
-}
+        if($debugQWMain) echo '<br />'.'existe pas else';
 
-    
-    
-    $requete = "SELECT AvancementUser FROM membres WHERE IdUser = ".$idCompte;
-    if($debugQWMain) echo $requete;
-    $ifAvancementUserExist = $pdo->query($requete);
-    if($debugQWMain) echo ''.$ifAvancementUserExist.'';
-    $AvancementUserExist = $ifAvancementUserExist->fetch(PDO::FETCH_ASSOC);
-    if($debugQWMain) echo '<br />'.$AvancementUserExist["AvancementUser"];
+
+
+
+
+        $modifdata = $pdo->prepare('UPDATE membres SET AvancementUser= :numAvance WHERE IdUser= :idCompte');
+        try
+        {
+            $numAvancement = 1;
+            //$modifdata->bindParam(':datedec', $_POST['datedec'], PDO:: PARAM_STR); // date
+            $modifdata->bindParam(':numAvance', $numAvancement, PDO:: PARAM_INT); // entier
+            $modifdata->bindParam(':idCompte', $idCompte, PDO:: PARAM_INT); // entier
+            $modifdata->execute();
+            //header ('location: avancementAventure.php');
+
+        }
+        catch ( Exception $e )
+        {
+
+            if($debugQWMain) echo 'Erreur de requête : ', $e->getMessage();
+            //header ('location: accueil.php');
+
+        }
 }
-    
-if($AvancementUserExist["AvancementUser"] > 0)
+  
+
+
+
+        $requete = "SELECT AvancementUser FROM membres WHERE IdUser = ".$idCompte;
+        if($debugQWMain) echo $requete;
+        $ifAvancementUserExist = $pdo->query($requete);
+
+        $AvancementUserExist = $ifAvancementUserExist->fetch(PDO::FETCH_ASSOC);
+        if($debugQWMain) echo '<br />'.$AvancementUserExist["AvancementUser"];
+
+
+
+
+if($AvancementUserExist["AvancementUser"] >= 0)
 { 
     
     if($debugQWMain) echo '<br />deuxième boucle > 0';
     
+    ///////////////////////////redirection avanture/////////////
     if($AvancementUserExist["AvancementUser"] == 1){
-        //redirection première
+            if($debugQWMain) echo '<br />'.'aventure 1';
+        //redirection bonne page aventure
+        //header ('location: page.php');
+    }else if($AvancementUserExist["AvancementUser"] == 2){
+            if($debugQWMain) echo '<br />'.'aventure 2';
+        //redirection bonne page aventure
+        //header ('location: page.php');
+    }else if($AvancementUserExist["AvancementUser"] == 3){
+            if($debugQWMain) echo '<br />'.'aventure 3';
+        //redirection bonne page aventure
         //header ('location: page.php');
     }
+    else{
+          if($debugQWMain) echo '<br />'.'aventure pas encore présente';
+        //redirection bonne page aventure
+        //header ('location: page.php');
+     }
+    
+    
     
     
 /*
