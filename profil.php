c2fa669c320b30ae87b4c93c3a8218a6de2e253b
Adrien LM <didi56450@gmail.com>
2019-03-21 15:50:57 +0100

    Page modif  profil

--- profil.php
+++ profil.php
@@ -60,7 +60,7 @@ if(isset($_SESSION['id']) && $_SESSION['id'] > 0)
             <div class="partie1">
 
                 <div class="banniere">
-
+                    <a href="editionProfil.php"><img class="iconeEdite" src="images/editionprofil.svg" alt="Editer le profil"></a>
                     <div class="imageProfil"><img class="imgProfil" src="membres/avatars/<?php echo $userinfo['AvatarUser']; ?>" alt="image du profil utilisateur"></div>
                     <div class="nomProfil">
                         <h4 class="nomProfil">
@@ -86,7 +86,7 @@ if(isset($_SESSION['id']) && $_SESSION['id'] > 0)
                         echo '<p>'.$userinfo['DescriptionUser'].'</p>'; 
     
    }else{
-        echo '<p>Editer description ?</p>';
+        echo '<p>Pas de description</p>';
     }
             ?>       
                     </div>
